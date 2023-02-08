d0e13b6434c12cbd72b40b02d6c9bd926b12e958
Aram Torosyan <aramtorosyan1999@gmail.com>
2023-02-08 01:25:23 -0800

    first two parser functions

--- TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
+++ TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
@@ -2,6 +2,7 @@
 # scrapy crawl tmdb_spider -o movies.csv
 
 import scrapy
+from scrapy.http import Request
 
 class TmdbSpider(scrapy.Spider):
     name = 'tmdb_spider'
@@ -14,8 +15,26 @@ class TmdbSpider(scrapy.Spider):
         passes it to another parser function.
         """
 
-        next_page = response.css('#media_v4.new_button a::attr(href)').get()
-        yield response.follow(next_page, callback=parse_full_credit)
+        # this command finds the url to the next page
+        next_page = response.css('#media_v4 .new_button a::attr(href)').get()
+        # joins the initial link to the url found above
+        yield response.follow(next_page, callback=self.parse_full_credit)
 
     def parse_full_credit(self, response):
+        """
+        This function extracts the links to each cast member and 
+        calls parse_actor_page function for further parsing.
+        """
+        # gets the links to the cast members
+        cast_links=response.css('.panel.pad > ol > li > a::attr(href)').getall()
+
+        # makes a full url link
+        next_pages = ["https://www.themoviedb.org" + link for link in cast_links]
+
+        # loops over actor pages
+        for next_page in next_pages:
+            yield Request(next_page, callback = self.parse_actor_page)
+
+    def parse_actor_page(self, response):
         pass
+        
